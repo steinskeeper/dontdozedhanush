4899cd0c73f1fa8ec65d420d48148f26eb57f373
steinskeeper <dpelpsykongroo@gmail.com>
2022-01-31 16:13:16 +0530

    first commit

---
+++ src/routes/Home.svelte
@@ -0,0 +1,16 @@
+
+<div class="container mx-auto h-screen flex flex-col justify-center items-center">
+  <h1>
+   
+    <strong class="bg-clip-text lg:text-7xl text-3xl text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+      WAKE DHANUCHU
+    </strong>
+  </h1>
+
+  <button class="bg-gradient-to-r from-orange-500 to-purple-500 mt-8 hover:bg-blue-200 text-white font-bold p-4 border-b-4 rounded-full w-48 hover:shadow-2xl">
+    Click here
+  </button>
+  <div class="mt-8 text-2xl font-semibold">Made with <span style="color: #e25555;">&hearts;</span> by Dhanush</div>
+
+ 
+</div>
